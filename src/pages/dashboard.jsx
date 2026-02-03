712a23b6b18b3a5d1863e6ded58415e85ef4acba
Blast <bahianlouiejay@gmail.com>
2026-02-03 16:23:53 +0800

    Save local changes before merge

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -18,6 +18,7 @@ function Dashboard() {
           <h1>Logging System</h1>
           <h2>MENU</h2>
         </div>
+        <div className="sidebar-divider"></div>
         <div className="dash-options">
           <div
             className={
